e70129e911ee6fa364250b703378a4df14ccafb4
Mohannad Mahmoud <mohannad_m@windowslive.com>
2023-09-11 18:09:58 +0300

    Both platforms working but not all flags supported

--- src/sources.mk
+++ src/sources.mk
@@ -11,7 +11,11 @@
 
 # Add your Source files to this variable
 SOURCES =	main.c \
-			memory.c
-
+			memory.c \
+			interrupts_msp432p401r_gcc.c \
+			startup_msp432p401r_gcc.c \
+			system_msp432p401r.c
 # Add your include paths to this variable
-INCLUDES = -I ../include/common
+INCLUDES = 	-I ../include/common \
+			-I ../include/CMSIS \
+			-I ../include/msp432
